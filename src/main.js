0ce472ca90cbb29aad8b9342699d3662e1242156
fanguangrui <898365387@qq.com>
2020-08-29 21:07:00 +0800

    1.设置默认路径 2.设置页面title

--- src/main.js
+++ src/main.js
@@ -6,7 +6,13 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(ElementUI);
 Vue.config.productionTip = false
-
+router.beforeEach((to, from, next) => {
+  /* 路由发生变化修改页面title */
+  if (to.meta.title) {
+    document.title = to.meta.title
+  }
+  next()
+})
 new Vue({
   el: '#app',
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -5,31 +5,68 @@ import home from "../components/home";
 import sort from "../components/sort"
 const routes = [
     {   path: '/',
+        redirect: '/sort/selection',//设置默认指向的路径
         component: home ,
+        meta:{
+            // 页面标题title
+            title: '首页'
+        },
         children: [
             {   path: 'sort'
                 ,component: sort
+                ,meta:{
+                    // 页面标题title
+                    title: '排序'
+                }
                 ,children: [
                     {   path: 'selection'
                         ,component: () => import('../components/sort/selection')
+                        ,meta:{
+                            // 页面标题title
+                            title: '选择排序'
+                        }
                     }
                     ,{   path: 'insertion'
                         ,component: () => import('../components/sort/insertion')
+                        ,meta:{
+                            // 页面标题title
+                            title: '插入排序'
+                        }
                     }
                     ,{   path: 'shell'
                         ,component: () => import('../components/sort/shell')
+                        ,meta:{
+                            // 页面标题title
+                            title: '希尔排序'
+                        }
                     }
                     ,{   path: 'merge'
                         ,component: () => import('../components/sort/merge')
+                        ,meta:{
+                            // 页面标题title
+                            title: '归并排序'
+                        }
                     }
                     ,{   path: 'mergeBU'
                         ,component: () => import('../components/sort/mergeBU')
+                        ,meta:{
+                            // 页面标题title
+                            title: '自底向上的归并排序'
+                        }
                     }
                     ,{   path: 'quick'
                         ,component: () => import('../components/sort/quick')
+                        ,meta:{
+                            // 页面标题title
+                            title: '快速排序'
+                        }
                     }
                     ,{   path: 'quick3way'
                         ,component: () => import('../components/sort/quick3way')
+                        ,meta:{
+                            // 页面标题title
+                            title: '三分快速排序'
+                        }
                     }
                 ]
             }
